d55c2e92b22533c8c91b6b36b121368604801ba8
Saumya Man Shrestha <saumyamanshrestha@gmail.com>
2025-04-27 15:44:54 +0545

    Introduction to JS

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-// alert("Hello, Happy New Year!");
+// alert("Hello, Happy Mothers Day!");
+// confirm("Are You Sure?");
+// prompt("Enter your age:");
+// localStorage.setItem("name", "Saumya");
+
+// const alert1 = alert("Hello, Happy Mothers Day!");
+// console.log("This is Alert", alert1);
+
+// let conf = confirm("Are You Sure?");
+// console.log(conf);
+
+let prm = prompt("Enter your age:");
+console.log("Your age is: ", prm);
