131e4574747c7d44ba27aae16203b316250692ea
Saumya Man Shrestha <saumyamanshrestha@gmail.com>
2025-04-28 15:44:30 +0545

    Variable Scopes

--- js/index.js
+++ js/index.js
@@ -1,13 +1,34 @@
-// alert("Hello, Happy Mothers Day!");
-// confirm("Are You Sure?");
-// prompt("Enter your age:");
-// localStorage.setItem("name", "Saumya");
+// Variable CSSStyleDeclaration
+// var a = 6; //Global Scope
+// let a = 5; //Block Scope
+// const a = 5; //Block Scope
 
-// const alert1 = alert("Hello, Happy Mothers Day!");
-// console.log("This is Alert", alert1);
+//Global Scope
+// var a = 5;
+// var a = 10;
+// {
+//   var a = 15;
+//   console.log("Value of block a is: ", a);
+// }
+// var a = 10;
+// console.log("Value of global a is: ", a);
 
-// let conf = confirm("Are You Sure?");
-// console.log(conf);
+//Block Scope
+// let b = 10;
+// let c = 20;
+// {
+//   let b = 40;
+//   console.log("Value of block b is: ", b);
+// }
+// console.log("Value of global b is: ", b);
+// console.log("Value of c is: ", c);
 
-let prm = prompt("Enter your age:");
-console.log("Your age is: ", prm);
+//Block Scope
+const d = 20;
+const e = 40;
+{
+  const d = 50;
+  console.log("Value of block d is: ", d);
+}
+console.log("Value of global d is: ", d);
+console.log("Value of e is: ", e);
