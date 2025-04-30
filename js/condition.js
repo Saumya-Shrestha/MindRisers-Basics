381d7f39bf2be4951aa04a5f9d7b4d90d0163d5a
Saumya Man Shrestha <saumyamanshrestha@gmail.com>
2025-04-30 15:54:04 +0545

    Conditions in JS

---
+++ js/condition.js
@@ -0,0 +1,11 @@
+let isRain = false;
+let scorchingSun = false;
+let willNotRain = true;
+
+if (isRain) {
+  console.log("Take Umbrella");
+} else if (scorchingSun) {
+  console.log("Take Umbrella");
+} else {
+  console.log("Don't Take Umbrella");
+}
