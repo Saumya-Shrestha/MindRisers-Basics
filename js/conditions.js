f8dbcc0ccf9c954ff4627957237c2c5c8570e29e
Saumya Man Shrestha <saumyamanshrestha@gmail.com>
2025-05-01 15:50:42 +0545

    Operator in JS

---
+++ js/conditions.js
@@ -0,0 +1,40 @@
+let isRain = false;
+let scorchingSun = false;
+let willNotRain = true;
+let today = "thursday";
+
+// if (isRain) {
+//   console.log("Take Umbrella");
+// } else if (scorchingSun) {
+//   console.log("Take Umbrella");
+// } else {
+//   console.log("Don't Take Umbrella");
+// }
+
+// let decision = isRain ? "Take Umbrella" : scorchingSun ? "Take Umbrella" : "Don't Take Umbrella";
+// console.log(decision);
+
+let decision = today == "friday" ? "Happy Weekend" : "Sad Week";
+console.log(decision);
+
+// if (today == "sunday") {
+//   console.log("Today is Sunday");
+// } else if (today == "monday") {
+//   console.log("Today is Monday");
+// } else if (today == "tuesday") {
+//   console.log("Today is Tuesday");
+// } else if (today == "wednesday") {
+//   console.log("Today is Wednesday");
+// } else if (today == "thursday") {
+//   console.log("Today is Thursday");
+// } else if (today == "friday") {
+//   console.log("Today is Friday");
+// } else {
+//   console.log("Today is Saturday");
+// }
+
+// let number = 4.5;
+// let num1 = Math.floor(number);
+// let num2 = Math.ceil(number);
+// console.log(num1);
+// console.log(num2);
