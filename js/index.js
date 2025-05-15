46ec6249a5a725294512fbf79805ef844d579c88
Saumya Man Shrestha <saumyamanshrestha@gmail.com>
2025-05-15 09:25:20 +0545

    HTML/JS DOM

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-// Variable CSSStyleDeclaration
 // var a = 6; //Global Scope
 // let a = 5; //Block Scope
 // const a = 5; //Block Scope
@@ -33,7 +32,29 @@
 // console.log("Value of global d is: ", d);
 // console.log("Value of e is: ", e);
 
-//Naming
-const name = "Ramesh";
-let title = "js";
-let number = 40;
+// Naming
+// const name = "Ramesh";
+// let title = "js";
+// let number = 40;
+
+//* HTML JS DOM
+const mainTitle = document.querySelector("h1");
+const allButtons = document.getElementsByTagName("button");
+const orderButtons = document.querySelectorAll(".btn-warning");
+const contactForm = document.querySelector(".section-two form");
+
+mainTitle.textContent = "Welcome to Grand Hotel";
+mainTitle.innerHTML = "Welcome to <em>Grand Hotel</em>";
+
+mainTitle.style.color = "#ff8800";
+mainTitle.style.fontSize = "3rem";
+
+const toggleBtn = document.querySelector(".toggle-btn");
+const toggleBtnIcon = document.querySelector(".toggle-btn i");
+const dropdownMenu = document.querySelector(".dropdown_menu");
+
+toggleBtn.addEventListener("click", function () {
+  dropdownMenu.classList.toggle("open");
+  const isOpen = dropdownMenu.classList.contains("open");
+  toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
+});
